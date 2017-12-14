576ce35a86ca3228ad82edf92baa200876767d9a
Hieu <hieuci88@gmail.com>
2017-12-14 11:21:04 +0100

    ToDo application: Print usage done

--- todo_application/todo/src/ToDo.java
+++ todo_application/todo/src/ToDo.java
@@ -1,2 +1,15 @@
+
 public class ToDo {
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("Command Line Todo application\n" +
+              "=============================\n" +
+              "\n" +
+              "Command line arguments:\n" +
+              " -l   Lists all the tasks\n" +
+              " -a   Adds a new task\n" +
+              " -r   Removes a task\n" +
+              " -c   Completes a task");
+    }
+  }
 }
